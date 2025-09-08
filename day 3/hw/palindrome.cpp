1923d9edadfe2442ad9f428a03dd088de9502d99
Nikhil-yadav-kumar <nikhilyadav72646@gmail.com>
2025-09-09 00:19:43 +0530

    hw done 1

---
+++ day 3/hw/palindrome.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num, original, rev= 0, remainder;
+    
+    cout << "Enter a number: ";
+    cin >> num;
+
+    original = num;
+    while (num != 0) {
+        remainder = num % 10;
+        rev = rev * 10 + remainder;
+        num /= 10;
+    }
+    
+    if (original == rev) {
+        cout << original << " is a Palindrome number." << endl;
+    } else {
+        cout << original << " is not a Palindrome number." << endl;
+    }
+
+    return 0;
+}
