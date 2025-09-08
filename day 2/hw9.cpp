f29149ce7fd170a1ae12d6843a124ed420ecfee0
Nikhil-yadav-kumar <nikhilyadav72646@gmail.com>
2025-09-08 16:37:00 +0530

    day 3 done

--- day 2/hw9.cpp
+++ day 2/hw9.cpp
@@ -8,7 +8,9 @@ int main() {
 
     if (marks < 0 || marks > 100) {
         cout << "Invalid marks!" << endl;
-    } else {
+    } 
+    
+    else {
         
         switch (marks / 10) {
             case 10: 
@@ -18,12 +20,17 @@ int main() {
                 cout << "Distinction" << endl;
                 break;
 
-            case 6:  
+            case 6: 
+                cout<<"first division "<<endl;
+                break; 
             case 5:  
             case 4:  
                 cout << "Pass" << endl;
                 break;
-
+            // case 3:
+            // case 2:
+            // case 1:
+            //     cout<<"fail"<<endl;
             default: 
                 cout << "Fail" << endl;
         }
